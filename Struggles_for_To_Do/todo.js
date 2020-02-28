ee0bc680c78fa3a753a25a30bd4ddf7cedacf581
Seo <jessjin01@gmail.com>
2020-02-28 22:15:08 +0900

    부트스트랩 적용

---
+++ Struggles_for_To_Do/todo.js
@@ -0,0 +1,14 @@
+function pressEnter(){
+    var input=document.getElementById("taskinput").value;
+    var listadd=documet.createElement("li");listadd.innerHTML=input;
+    document.getElementById()
+
+}
+
+function edit(){
+
+}
+
+function del(){
+
+}
